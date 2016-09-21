caf8131ce9b330174a0e249e10f09f6c7177c64a
SurayaDev <becky@codelouisville.org>
2016-09-21 01:04:35 -0400

    Finished testing Hotjar and refactored for new builder

--- test/index.test.js
+++ test/index.test.js
@@ -1,17 +1,15 @@
-var Analytics = require('analytics.js-core').constructor;
-var integration = require('analytics.js-integration');
-var sandbox = require('clear-env');
-var tester = require('analytics.js-integration-tester');
+var Analytics = require('@segment/analytics.js-core').constructor;
+var integration = require('@segment/analytics.js-integration');
+var sandbox = require('@segment/clear-env');
+var tester = require('@segment/analytics.js-integration-tester');
 var Hotjar = require('../lib/');
 
 describe('Hotjar', function() {
   var analytics;
   var hotjar;
   var options = {
-    events: {
         hjid: '250523',
         hjsv: '5'
-    }
   };
 
   beforeEach(function() {
@@ -31,9 +29,39 @@ describe('Hotjar', function() {
 
   it('should have the correct settings', function() {
     analytics.compare(Hotjar, integration('Hotjar')
+      .assumesPageview()
+      .global('hj')
+      .global('_hjSettings')
       .option('hjid')
       .option('hjsv'));
   });
+
+  describe('before loading', function() {
+    beforeEach(function() {
+      analytics.stub(hotjar, 'load');
+    });
+
+    describe('#initialize', function() {
+      it('should pass options to _hjSettings', function() {
+        analytics.initialize();
+        analytics.page();
+        analytics.equal(window._hjSettings.hjid, options.hjid);
+        analytics.equal(window._hjSettings.hjsv, options.hjsv);
+      });
+
+      it('should call #load', function() {
+        analytics.initialize();
+        analytics.page();
+        analytics.called(hotjar.load);
+      });
+    });
+  });
+
+  // describe('loading', function() {
+  //   it('should load', function(done) {
+  //     analytics.load(hotjar, done);
+  //   });
+  // });
 
   describe('after loading', function() {
     beforeEach(function(done) {
